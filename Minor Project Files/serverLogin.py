a1e129e339cbbe37096bdb8116c461bdf98a63cf
Mohit Mahajan <73893201+mohitmahajan095@users.noreply.github.com>
2024-05-06 18:02:33 +0530

    Update serverLogin.py

--- Minor Project Files/serverLogin.py
+++ Minor Project Files/serverLogin.py
@@ -3,10 +3,10 @@ import pyodbc
 def connect():
     try:
         connection = pyodbc.connect("DRIVER={SQL Server};"+
-                                "Server=ANONYMOUS_\SQLEXPRESS;"+
-                                "Database=USER_DATABASE;"+
+                                "Server=Your_Server_Name;"+
+                                "Database=YOUR_DATABASE_NAME;"+
                                 "Trusted_Connection=True")
-        print("Connected to SQL Server --> [ anonymous_\sqlexpress ]\n" + "Database : USER_DATABASE ") 
+        print("Connection Successfully Estiblished to MS-SQL-Server !") 
         return connection
         
     except pyodbc.Error as ex:
